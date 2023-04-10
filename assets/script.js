cdf650fd618d5a06d984fa267f36fafc17d06fe7
retnodamayanti <mayahere5@gmail.com>
2023-04-10 14:53:58 +0930

    updated questionsEl var and added event listener so 3rd question can be load

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var body = document.body;
 var highScore = document.createElement("h1");
 var timerEl = document.createElement("h2");
 // variable for element questions
-var divEL = document.createElement("div");
+var divEL = document.getElementById("choices");
 // variable for element answer options
 var answerOpt = document.createElement("ol")
 var answerList = [];
@@ -17,25 +17,24 @@ answerList[3]= document.createElement("li");
 // var answerList4 = document.createElement("li");
 
 var questionNumber = 0;
-var questionsEl = JSON.parse(
- `[
+var questionsEl = [
     {
-        "question": "Commonly used data types DO NOT Include:",
-        "mcq": ["strings", "booleans", "alerts", "numbers"],
-        "correctAnswer": "alerts"    
-    },
-    {   
-        "question": "Arrays in JavaScript can be used to store ____.:",
-        "mcq": ["number and strings", "other arrays", "bolean", "all of the above"],
-        "correctAnswer": "all of the above"
-        
+      question: "Commonly used data types DO NOT Include:",
+      mcq: ["strings", "booleans", "alerts", "numbers"],
+      correctAnswer: "alerts",
     },
     {
-        "question": "String values must be enclosed within _____ when being assigned to variables.",
-        "mcq": ["comma", "curly brackets", "quotes", "parentheses"],
-        "correctAnswer": "quotes"
-    }
-]`)
+      question: "Arrays in JavaScript can be used to store ____.:",
+      mcq: ["number and strings", "other arrays", "bolean", "all of the above"],
+      correctAnswer: "all of the above",
+    },
+    {
+      question:
+        "String values must be enclosed within _____ when being assigned to variables.",
+      mcq: ["comma", "curly brackets", "quotes", "parentheses"],
+      correctAnswer: "quotes",
+    },
+  ];
 
 // add text to the created element 
 highScore.textContent = "View Highscores";
@@ -90,25 +89,47 @@ function renderQuestion(index) {
    
 }
 
-function addEventListeners (){
-    qIndex = qIndex + 1;
-    answerList[0].addEventListener('click', function(event){
-       
-        renderQuestion(qIndex)
-    })
-    answerList[1].addEventListener('click', function(event){
-        
-        renderQuestion(qIndex)
-    })
-    answerList[2].addEventListener('click', function(event){
-        
-        renderQuestion(qIndex)
-    })
-    answerList[3].addEventListener('click', function(event){
-        
-        renderQuestion(qIndex)
-    })
-}
-
                 renderQuestion(0);
-                addEventListeners();
+                // addEventListeners();
+                divEL.addEventListener("click", function() {
+                   
+                    index = 1;
+                    divEL.textContent =  getQuestion(index)
+                        answerList[0].textContent = questionsEl[index].mcq[0]
+                        answerList[1].textContent = questionsEl[index].mcq[1]
+                        answerList[2].textContent = questionsEl[index].mcq[2]
+                        answerList[3].textContent = questionsEl[index].mcq[3]
+
+                        answerOpt.appendChild(answerList[0]);
+                        answerOpt.appendChild(answerList[1]);
+                        answerOpt.appendChild(answerList[2]);
+                        answerOpt.appendChild(answerList[3]);
+
+
+                        body.appendChild(highScore);
+                        body.appendChild(timerEl);
+                        body.appendChild(divEL);
+                        divEL.appendChild(answerOpt);
+                
+                        divEL.addEventListener("click", function(){
+                            index = 2;
+                            divEL.textContent =  getQuestion(index)
+                        answerList[0].textContent = questionsEl[index].mcq[0]
+                        answerList[1].textContent = questionsEl[index].mcq[1]
+                        answerList[2].textContent = questionsEl[index].mcq[2]
+                        answerList[3].textContent = questionsEl[index].mcq[3]
+
+                        answerOpt.appendChild(answerList[0]);
+                        answerOpt.appendChild(answerList[1]);
+                        answerOpt.appendChild(answerList[2]);
+                        answerOpt.appendChild(answerList[3]);
+
+
+                        body.appendChild(highScore);
+                        body.appendChild(timerEl);
+                        body.appendChild(divEL);
+                        divEL.appendChild(answerOpt);
+
+                });
+
+                });
